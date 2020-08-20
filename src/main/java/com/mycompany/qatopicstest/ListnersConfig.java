50c073462f78e077d102a138ff6c3049cc731634
Himanshu Singh <you@example.com>
2020-08-20 10:30:57 +0530

    First Commit

---
+++ src/main/java/com/mycompany/qatopicstest/ListnersConfig.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.qatopicstest;
+
+/**
+ *
+ * @author hp
+ */
+public class ListnersConfig implements ITestListener{
+    
+}
---
+++ src/test/java/HandlingDropDownCombobox.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.testng.Assert.*;
+import org.testng.ITestListener;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+
+/**
+ *
+ * @author hp
+ */
+@Listeners(ITestListener.class)
+public class HandlingDropDownCombobox {
+    
+    
+}
